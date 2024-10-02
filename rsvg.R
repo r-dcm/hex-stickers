1ffbdee07ad8c8adcaf6f78f38516e8c93690fde
Jake Thompson <wjakethompson@gmail.com>
2024-10-02 15:33:36 -0500

    add r-dcm hex

--- rsvg.R
+++ rsvg.R
@@ -13,19 +13,29 @@ sizes <- data.frame(
 subset(sizes, height != 2911)
 
 # Identify missing svg or png files --------------------------------------------
-png <- dir_ls("png", glob = "*.png")
+png_hires <- dir_ls("png-hires", glob = "*.png")
+png_lowres <- dir_ls("png-lowres", glob = "*.png")
 svg <- dir_ls("svg", glob = "*.svg")
 
-png_name <- path_ext_remove(path_file(png))
+png_hires_name <- path_ext_remove(path_file(png_hires))
+png_lowres_name <- path_ext_remove(path_file(png_lowres))
 svg_name <- path_ext_remove(path_file(svg))
-setdiff(png_name, svg_name)
-setdiff(svg_name, png_name)
+setdiff(png_hires_name, svg_name)
+setdiff(png_lowres_name, svg_name)
+(missing_hi <- setdiff(svg_name, png_hires_name))
+(missing_low <- setdiff(svg_name, png_lowres_name))
 
 # Convert svg to png -----------------------------------------------------------
 if (!dir_exists("png-lowres")) dir_create("png-lowres")
-out <- path("png-lowres", path_ext_set(path_file(svg), "png"))
-walk2(svg, out, rsvg_png, width = 250, height = 288.6751)
+if (length(missing_low) > 0) {
+  out <- path("png-lowres", path_ext_set(path_file(missing_low), "png"))
+  walk2(path("svg", path_ext_set(path_file(missing_low), "svg")), out,
+        rsvg_png, width = 250, height = 288.6751)
+}
 
 if (!dir_exists("png_hires")) dir_create("png-hires")
-out <- path("png-hires", path_ext_set(path_file(svg), "png"))
-walk2(svg, out, rsvg_png, width = 2521, height = 2911)
+if (length(missing_hi) > 0) {
+  out <- path("png-hires", path_ext_set(path_file(missing_hi), "png"))
+  walk2(path("svg", path_ext_set(path_file(missing_hi), "svg")), out,
+        rsvg_png, width = 2521, height = 2911)
+}
